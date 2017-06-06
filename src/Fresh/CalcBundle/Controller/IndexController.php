b0d67a0a90cfad03599196701e6fff084af430d2
freshcalc <freshcalc@web431.default-host.net>
2017-06-06 10:56:50 +0300

    Some changes on the server

--- src/Fresh/CalcBundle/Controller/IndexController.php
+++ src/Fresh/CalcBundle/Controller/IndexController.php
@@ -140,50 +140,190 @@ class IndexController extends Controller
 
         $pdf = $this->get("white_october.tcpdf")->create('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-        $fontname1 = TCPDF_FONTS::addTTFfont( $_SERVER['DOCUMENT_ROOT'].'fonts/PTC75F.ttf', 'TrueTypeUnicode', '', 96);
+        $fontname1 = TCPDF_FONTS::addTTFfont( $_SERVER['DOCUMENT_ROOT'].'web/fonts/PTC75F.ttf', 'TrueTypeUnicode', '', 96);
 
         $pdf->SetFont($fontname1, '', 14, '', false);
 
-
+//Коммерческое предложение заголовок (1 стр.)
         $pdf->AddPage();
         $bMargin = $pdf->getBreakMargin();
         $auto_page_break = $pdf->getAutoPageBreak();
         $pdf->SetAutoPageBreak(false, 0);
-        $img_file = $_SERVER['DOCUMENT_ROOT'].'images/commerce_sugestion_1_page.jpg';
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/commerce_sugestion_1_page.jpg';
         $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
         $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
         $pdf->writeHTMLCell( 100, $h = 0, 50, 116, '<p style="font-weight: bold;font-size: 13px;color: #222222;">по разработке сайта</p>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
         $pdf->writeHTMLCell( 100, $h = 0, 100, 116, '<p style="font-weight: bold;font-size: 13px;color: #222222;">'.$companyName.'</p>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
 
-
+//Приветствие клиента (2 стр.)
         $pdf->AddPage();
         $bMargin = $pdf->getBreakMargin();
         $auto_page_break = $pdf->getAutoPageBreak();
         $pdf->SetAutoPageBreak(false, 0);
-        $img_file = $_SERVER['DOCUMENT_ROOT'].'images/commerce_sugestion_2_page.jpg';
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/commerce_sugestion_2_page.jpg';
         $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
         $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
         $pdf->writeHTMLCell( 100, $h = 0, 220, 40, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px;text-decoration: none"><p style="font-weight: bold;font-size: 13px;color: #222222;line-height: 0.95">УЗНАЙТЕ О НАС БОЛЬШЕ<br/>ВСЕГО ЗА 2 МИНУТЫ</p></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
-        $pdf->writeHTMLCell( 100, 50, 220, 55, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">https://www.youtu.be/7NB4_TBkGkc</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
-        $pdf->writeHTMLCell( 100, 50, 199, 38, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><img src="'.$_SERVER['DOCUMENT_ROOT'].'images/watch.png" alt=""></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+        $pdf->writeHTMLCell( 100, 50, 220, 55, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">https://www.youtu.be/7NB4_TBkGkc</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+        $pdf->writeHTMLCell( 100, 50, 199, 38, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><img src="'.$_SERVER['DOCUMENT_ROOT'].'web/images/watch.png" alt=""></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
         $pdf->writeHTMLCell( '' , $h = 0, 25, 20, '<p style="font-weight: bold;font-size: 28px;color: #F59D0C;text-transform: uppercase">ЗДРАВСТВУЙТЕ, '.$name.'</p>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
 
-
+//Страница о нас с ссылками (3 стр.)
         $pdf->AddPage();
         $bMargin = $pdf->getBreakMargin();
         $auto_page_break = $pdf->getAutoPageBreak();
         $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/diagonal_links.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
 
         $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
-        $pdf->writeHTMLCell( '', 210, 0, 0, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><img src="'.$_SERVER['DOCUMENT_ROOT'].'images/diagonal_link_2.png" alt=""></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+        $pdf->Link(0, 0, 149, 210, 'https://youtu.be/7NB4_TBkGkc');
+        $pdf->Link(149, 0, 149, 210, 'https://youtu.be/Fh9TsoCk7DY');
+        $pdf->Link(142, 79, 52, 52, 'http://fresh-d.biz/');
+
+//Сайт Агро-Союз http://agroritet.com (4 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $pdf->SetMargins( 0, 0 , 0 );
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/agroritet_com.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->writeHTMLCell( 75, 10, 10, 173, '<a href="http://agroritet.com" style="display: block;font-size: 12px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://agroritet.com</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = false);
+
+//Сайт Liberta http://www.liberta.com.ua (5 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $pdf->SetMargins( 0, 0 , 0 );
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/liberta_com.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->writeHTMLCell( 75, 10, 10, 173, '<a href="http://www.liberta.com.ua" style="display: block;font-size: 12px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.liberta.com.ua</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = false);
+
+//Сайт Auremo http://www.auremo.org (6 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $pdf->SetMargins( 0, 0 , 0 );
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/auremo_org.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->writeHTMLCell( 75, 10, 10, 173, '<a href="http://www.auremo.org" style="display: block;font-size: 12px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.auremo.org</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = false);
+        $pdf->SetMargins( 0, 0 , 0 );
+
+//Сайт Агро-Союз (7 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetMargins( 0, 0 , 0 );
+        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/agrounion.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        //$pdf->writeHTMLCell( 75, 10, 10, 180, '<a href="http://www.auremo.org" style="display: block;font-size: 12px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.auremo.org</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Клиентский лист (8 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/client_list.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+        $pdf->writeHTMLCell( 180, 40, 222.5, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Смета по разработке (9 стр.)
+//        $pdf->AddPage();
+//        $bMargin = $pdf->getBreakMargin();
+//        $auto_page_break = $pdf->getAutoPageBreak();
+//        $pdf->SetAutoPageBreak(false, 0);
+//        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/why_we.jpg';
+//        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+//        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+//        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+//        $pdf->writeHTMLCell( 180, 40, 223, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Почему мы? (10 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/why_we.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->writeHTMLCell( 180, 40, 111, 139, '<a href="http://www.fresh-d.net/portfolio" style="display: block;font-size: 11px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">www.fresh-d.net/portfolio</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Доп. услуги (11 стр.)
+//        $pdf->AddPage();
+//        $bMargin = $pdf->getBreakMargin();
+//        $auto_page_break = $pdf->getAutoPageBreak();
+//        $pdf->SetAutoPageBreak(false, 0);
+//        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/client_list.jpg';
+//        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+//        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+//        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+//        $pdf->writeHTMLCell( 180, 40, 223, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Почему стоимость именно такая (12 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/why_price_like_this.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+//        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+//        $pdf->writeHTMLCell( 180, 40, 223, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+
+//Сроки выполнения работ (13 стр.)
+//        $pdf->AddPage();
+//        $bMargin = $pdf->getBreakMargin();
+//        $auto_page_break = $pdf->getAutoPageBreak();
+//        $pdf->SetAutoPageBreak(false, 0);
+//        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/client_list.jpg';
+//        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+//        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+//        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+//        $pdf->writeHTMLCell( 180, 40, 223, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//О системе управления сайтом (14 стр.)
+        $pdf->AddPage();
+        $bMargin = $pdf->getBreakMargin();
+        $auto_page_break = $pdf->getAutoPageBreak();
+        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/about_content_management_system-01.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+//        $pdf->Link(200, 40, 65, 20, 'http://www.fresh-d.net/studio/regals.html');
+//        $pdf->writeHTMLCell( 180, 40, 223, 60, '<a href="https://youtu.be/YhNySK73C6o" style="display: block;font-size: 7px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">http://www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+
+//Контактная информация (15 стр.)
+        $pdf->AddPage();
+//        $bMargin = $pdf->getBreakMargin();
+//        $auto_page_break = $pdf->getAutoPageBreak();
+//        $pdf->SetAutoPageBreak(false, 0);
+        $img_file = $_SERVER['DOCUMENT_ROOT'].'web/images/contact_information-01.jpg';
+        $pdf->Image($img_file, 0, 0, 297, 210, '', '', '', false, 1300, '', false, false, 0);
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 058', PDF_HEADER_STRING, array(0,0,0), array(255,255,255));
+        $pdf->SetPrintHeader(false);
+        $pdf->SetPrintFooter(false);
+        
+//        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        $pdf->SetMargins( -20 , -20 , -20 , true );
+        $pdf->writeHTMLCell( 200, 10, 177, 116, '<a href="https://youtu.be/7NB4_TBkGkc" style="display: block;font-size: 9px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">https://youtu.be/7NB4_TBkGkc</div></a>', $border = 0, $ln = 1, $fill = 0, false, $align = '', false);
+        $pdf->writeHTMLCell( 200, 10, 177, 134, '<a href="http://www.fresh-d.net/" style="display: block;font-size: 9px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">www.fresh-d.net</div></a>', $border = 0, $ln = 1, $fill = 0, false, $align = '', false);
+        $pdf->writeHTMLCell( 180, 10, 177, 151, '<a href="http://www.fresh-d.net/studio/regals.html" style="display: block;font-size: 9px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">www.fresh-d.net/studio/regals.html</div></a>', $border = 0, $ln = 1, $fill = 0, false, $align = '', false);
+        $pdf->writeHTMLCell( 180, 10, 177, 167, '<a href="https://youtu.be/Fh9TsoCk7DY" style="display: block;font-size: 9px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">https://youtu.be/Fh9TsoCk7DY</div></a>', $border = 0, $ln = 1, $fill = 0, false, $align = '', false);
 
 
 
 
-        $pdf->writeHTMLCell( 100, $h = 0, 220, 40, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px;text-decoration: none"><p style="font-weight: bold;font-size: 13px;color: #222222;line-height: 0.95">УЗНАЙТЕ О НАС БОЛЬШЕ<br/>ВСЕГО ЗА 2 МИНУТЫ</p></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
-        $pdf->writeHTMLCell( 100, 50, 220, 55, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><div style="width: 250px;height: 250px; padding: 250px;">https://www.youtu.be/7NB4_TBkGkc</div></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
-        $pdf->writeHTMLCell( 100, 50, 199, 38, '<a href="https://www.youtu.be/7NB4_TBkGkc" style="display: block;font-size: 5px; padding: 25px;"><img src="'.$_SERVER['DOCUMENT_ROOT'].'images/watch.png" alt=""></a>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
-        $pdf->writeHTMLCell( '' , $h = 0, 25, 20, '<p style="font-weight: bold;font-size: 28px;color: #F59D0C;text-transform: uppercase">ЗДРАВСТВУЙТЕ, '.$name.'</p>', $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
 
 
 
@@ -193,7 +333,8 @@ class IndexController extends Controller
 //        $pdf->Write(10, 'Google', 'http://www.google.com/', false, 'L', true);
 
 
-        $filename = 'pdf_files/commerce_sugestion_'.time().'.pdf';
+        $filename = 'web/pdf_files/commerce_sugestion_'.time().'.pdf';
+        //var_dump($filename);die();
         $pdf->Output( $_SERVER['DOCUMENT_ROOT'].''.$filename,'F');
 
         return $filename;
